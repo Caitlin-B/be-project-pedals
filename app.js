8b7e1c35668d84b4eaf4906f88863b0ef37e8b9e
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-03-13 10:41:33 +0000

    changed db path

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const db = require("/Users/caitlinbishop/Desktop/Coding/project/project-pedals/be-project-pedals/db/index");
+const db = require("./db/index");
 const apiRouter = require("./routers/api.router");
 const { handleCustomError, typeErrorHandler } = require("./errors/index");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
